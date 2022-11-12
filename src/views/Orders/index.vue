971e6865b33356af44107d8d75615131f28f59bb
huangjq148 <583075772@qq.com>
2022-11-13 00:01:09 +0800

    添加导出功能

--- src/views/Orders/index.vue
+++ src/views/Orders/index.vue
@@ -8,6 +8,8 @@ import { orderFilterOptions, orderStatusOptions } from "@/data";
 import { getList, remove, update } from "@/utils/storage";
 import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
+import { utils, writeFile } from "xlsx"
+import dayjs from "dayjs"
 
 const value = ref("");
 const dataSource = ref(getList("orders"));
@@ -45,10 +47,71 @@ const jumpToEdit = (id) => {
 const jumpToOverview = () => {
   router.push("/orders/overview")
 }
+
+const addProcessingOrderData = (workbook) => {
+  const jsonData = dataSource.value.filter(item => item.status == "processing").map(item => ({
+    "id": item.id,
+    "date": item.date,
+    "goodsName": item.goodsName,
+    "number": item.number,
+    "remark": item.remark,
+    "person": item.person,
+    "address": item.address,
+    "sellPrice": item.sellPrice,
+    "buyPrice": item.buyPrice,
+  }))
+  const sheetHeader = ["id", "date", "goodsName", "number", "remark", "person", "address", "sellPrice", "buyPrice"]
+  const headerDisplay = { id: "订单号", date: "日期", goodsName: "产品", number: "重量（斤）", remark: "特殊要求", person: "收货人", address: "收货地址", sellPrice: "单价", buyPrice: "进价" }
+  var sheet = utils.json_to_sheet([headerDisplay, ...jsonData], { skipHeader: true, header: sheetHeader })
+  sheet['!cols'] = [{ width: 20 }, { width: 15 }, { width: 20 }, { width: 10 }, { width: 15 }, { width: 15 }, { width: 40 }, { width: 10 }, { width: 10 }];
+  utils.book_append_sheet(workbook, sheet, "送货单")
+}
+
+const addOrderData = (workbook) => {
+  const jsonData = dataSource.value.filter(item => item.status == "completed").map(item => ({
+    "id": item.id,
+    "date": item.date,
+    "goodsName": item.goodsName,
+    "number": item.number,
+    "sellPrice": item.sellPrice,
+    "sellTotal": item.sellPrice * item.number,
+    "buyPrice": item.buyPrice,
+    "buyTotal": item.buyPrice * item.number,
+    "profit": item.sellPrice * item.number - item.buyPrice * item.number
+  }))
+  const sheetHeader = ["id", "date", "goodsName", "number", "sellPrice", "sellTotal", "buyPrice", "buyTotal", "profit"]
+  const headerDisplay = { id: "订单号", date: "日期", goodsName: "产品", number: "重量（斤）", sellPrice: "单价", sellTotal: "实收", buyPrice: "进价", buyTotal: "进价总价", "profit": "毛利润" }
+  var sheet = utils.json_to_sheet([headerDisplay, ...jsonData], { skipHeader: true, header: sheetHeader })
+  sheet['!cols'] = [{ width: 20 }, { width: 15 }, { width: 20 }, { width: 10 }, { width: 10 }, { width: 10 }, { width: 10 }, { width: 10 }, { width: 10 }];
+  utils.book_append_sheet(workbook, sheet, "收款")
+}
+
+const addGoodsData = (workbook) => {
+  const jsonData = getList("goods").map(item => ({
+    "name": item.name,
+    "buyPrice": item.buyPrice,
+    "sellPrice": item.sellPrice
+  }))
+
+  const sheetHeader = ["name", "buyPrice", "sellPrice"]
+  const headerDisplay = { "name": "品名", buyPrice: "成本/1斤", sellPrice: "售价/1斤" }
+  var sheet = utils.json_to_sheet([headerDisplay, ...jsonData], { skipHeader: true, header: sheetHeader })
+  sheet['!cols'] = [{ width: 10 }, { width: 10 }, { width: 10 }];
+  utils.book_append_sheet(workbook, sheet, "产品")
+}
+
+const onExportClick = () => {
+  var workbook = utils.book_new();
+
+  addProcessingOrderData(workbook)
+  addOrderData(workbook)
+  addGoodsData(workbook)
+  writeFile(workbook, `渔-${dayjs().format('YYYYMMDD')}.xlsx`);
+}
 </script>
 
 <template>
-  <van-nav-bar title="全部订单" />
+  <van-nav-bar title="全部订单" right-text="导出" @click-right="onExportClick" />
   <van-search v-model="value" placeholder="请输入搜索关键词" show-action>
     <template #action>
       <div @click="jumpToOverview">汇总</div>
