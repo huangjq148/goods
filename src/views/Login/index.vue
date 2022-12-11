de380cc601fe52172e4d020efbd4988b635af20b
huangjq148 <583075772@qq.com>
2022-12-11 15:48:00 +0800

    登陆页面优化

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,6 @@
 <template>
     <van-form @submit="onSubmit">
+        <div class="login-header">登陆</div>
         <van-cell-group inset>
             <van-field v-model="username" name="username" label="用户名" placeholder="用户名"
                 :rules="[{ required: true, message: '请填写用户名' }]" />
@@ -18,8 +19,7 @@
 import { ref } from 'vue';
 import { login } from "../../services/user"
 import { setToken } from "../../utils/auth"
-import { useRouter, useRoute } from 'vue-router'
-
+import { useRouter } from 'vue-router'
 
 const $router = useRouter()
 const username = ref('');
@@ -33,3 +33,12 @@ const onSubmit = async (values) => {
 
 };
 </script>
+
+<style>
+.login-header {
+    text-align: center;
+    padding: 20px 0;
+    font-size: 30px;
+    font-weight: 400;
+}
+</style>
